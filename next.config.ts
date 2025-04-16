4ab98ea80cbdec050aa1620c6d26ca82657cade1
Johcel Gene T. Bitara • BS IT <jbitara@gbox.adnu.edu.ph>
2025-04-16 23:59:01 +0800

    fix: config build.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/ascii" : "",
   output: "export",
   images: {
     unoptimized: true,
